b2960cba753956d1e9382d6a080f9e2575fb2ea4
hxwan <260828082@qq.com>
2020-02-02 14:00:12 +0800

    项目结构创建完成...

---
+++ gmall-oms/src/test/java/edu/shu/gmall/oms/GmallOmsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.shu.gmall.oms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallOmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
